7d117ca8b0756da1c4af9548379655302a1aa923
Timothy Holmes <tim.a.holmes@gmail.com>
2024-09-13 06:37:24 +1000

    tidy startup_host.sh

--- file-server/startup_host.sh
+++ file-server/startup_host.sh
@@ -1,8 +1,8 @@
-docker compose down --remove-orphans && sudo rm -rf files & docker compose up --build -d
+docker compose down --remove-orphans && sudo rm -rf files & rm -rf seed_files.txt & docker compose up --build -d
 
 # for each file in files, do curl command
 for file in ./files/*; do
     echo $file >> ./seed_files.txt
     curl -d "$(cat $file)" http://localhost:4000 >> ./seed_files.txt
-    echo "" >> ./seed_files.txt
+    echo "\n" >> ./seed_files.txt
 done
