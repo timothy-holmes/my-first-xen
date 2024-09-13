031be24980b4fdb48bf25207738075fcf6d0f756
Timothy Holmes <tim.a.holmes@gmail.com>
2024-09-13 10:43:30 +1000

    add Bearer token file server

--- file-server/startup_host.sh
+++ file-server/startup_host.sh
@@ -1,8 +1,17 @@
+# Run this script with (export $(cat .env | xargs) && sh ./startup_host.sh)
+
+# Stops execution if env vars not set
+set -u # or set -o nounset
+: "$WRITE_SHARED_KEY"
+
 docker compose down --remove-orphans && sudo rm -rf files & rm -rf seed_files.txt & docker compose up --build -d
 
 # for each file in files, do curl command
 for file in ./files/*; do
     echo $file >> ./seed_files.txt
-    curl -d "$(cat $file)" http://localhost:4000 >> ./seed_files.txt
+    curl \
+        -d "$(cat $file)" \
+        -X POST -H "Authorization: Bearer $WRITE_SHARED_KEY" \
+        https://sfs.cloud.timothyholmes.com.au >> ./seed_files.txt
     echo "\n" >> ./seed_files.txt
 done
