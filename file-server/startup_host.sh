04ecfb293e02da8f4dcf45fc14d18b08984b1cf1
Timothy Holmes <tim.a.holmes@gmail.com>
2024-09-13 12:16:03 +1000

    add uuids to preseed.cfg

--- file-server/startup_host.sh
+++ file-server/startup_host.sh
@@ -6,16 +6,15 @@ set -u # or set -o nounset
 
 # Clean up previous run
 docker compose down --remove-orphans
--sudo rm -rf files & rm -rf seed_files.txt
 
 # Let's gooo
 docker compose up --build -d
 
-sleep 20
+# sleep 20
 
-# for each file in files, do curl command
-docker exec -it mfx-file-server sh -c ' \
-    echo "WRITE_SHARED_KEY" >> ./seed_files.txt; \
-    curl -d "$(echo "$WRITE_SHARED_KEY")" -X POST -H "Authorization: Bearer $WRITE_SHARED_KEY" https://sfs.cloud.timothyholmes.com.au >> ./seed_files.txt; \
-    echo "" >> ./seed_files.txt; \
-    cat ./seed_files.txt'
+# # push write key
+# docker exec -it mfx-file-server sh -c ' \
+#     echo "WRITE_SHARED_KEY" >> ./seed_files.txt; \
+#     curl -d "$(echo "$WRITE_SHARED_KEY")" -X POST -H "Authorization: Bearer $WRITE_SHARED_KEY" https://sfs.cloud.timothyholmes.com.au >> ./seed_files.txt; \
+#     echo "" >> ./seed_files.txt; \
+#     cat ./seed_files.txt'
